5d3d35c9e3b00dcf55d0119262504ade74e71e7d
Aalekh Nigam <aalekh1993@rediffmail.com>
2013-12-10 23:44:58 +0530

    Update index.php
    Improved code

--- index.php
+++ index.php
@@ -31,10 +31,47 @@ include 'output.html.php';
 exit();
 }
 
-while ($row = $result->fetch())
-{ 
-  $events[] = array('id' => $row['id'], 'title' => $row['title'],'description' => $row['description']);
+$b=0;
+$d=cal_days_in_month(CAL_GREGORIAN,12,2013);
+$emptyArray = array(array());
+
+while ($b!=$d) {
+
+  $tomorrow = mktime(0,0,0,date("m"),date("d")+$b,date("Y")); 
+  $date=date('Y-m-d',$tomorrow);
+  $indivisualdate=date('d');
+  $timestamp = strtotime($date);
+  $day = date('l', $timestamp);
+  $emptyArray[$b]=$day;
+  $b=$b+1;
+  
 }
 
+while ($row = $result->fetch())
+{ 
+  $events[] = array('id' => $row['id'], 'title' => $row['title'],'description' => $row['description'],'start_date' => $row['start_date'],'end_date' => $row['end_date']);
+}
+$b=0;
+$emptyArrayTwo=array();
+while($b!=$d-1)
+{
+  $emptyArrayTwo[$b]="";
+  $b=$b+1;
+}
+$b=0;
+while ($b!=$d-1) {
+  foreach ($events as $event): 
+   if($event['start_date']==$b+1)
+    {
+      
+      $emptyArrayTwo[$b]=$event['title'];
+      
+    }
+  endforeach; 
+  $b=$b+1;
+}
+
+$b=0;
 $output= 'database connection established';
 include 'output.html.php';  
+  
