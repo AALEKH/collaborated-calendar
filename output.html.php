2ad505147b66d8f0826960f8a892bf038e573808
Aalekh Nigam <aalekh1993@rediffmail.com>
2013-12-10 23:47:35 +0530

    Update output.html.php
    Improved Code

--- output.html.php
+++ output.html.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
@@ -5,18 +6,22 @@
 </head>
 <body>
 <p>
-<?php echo $output; ?>
-<br>
-<?php
-$d=cal_days_in_month(CAL_GREGORIAN,12,2013);
-echo "$d ";
-?>
+
 <br>
 
-<?php foreach ($events as $event): 
- echo htmlspecialchars($event['title'], ENT_QUOTES, 'UTF-8')."<br>";
- echo htmlspecialchars($event['description'], ENT_QUOTES, 'UTF-8');
- endforeach; ?>
+<br>
+<?php 
+while($b!=$d-1)
+{
+  echo $b+1;
+  echo  htmlspecialchars($emptyArray[$b], ENT_QUOTES, 'UTF-8')."<br>";
+  echo  htmlspecialchars($emptyArrayTwo[$b], ENT_QUOTES, 'UTF-8')."<br>"."<br>";
+
+  $b=$b+1;
+}
+ ?>
 </p>
 </body>
 </html>
+
+
